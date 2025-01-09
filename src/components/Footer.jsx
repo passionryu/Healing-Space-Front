9e95d32ede90ae06b6ecddb4f2bf9c18f2298568
passionryu <rsy1225@naver.com>
2025-01-09 20:21:20 +0900

    [feat] 메인 페이지 기본 헤더,바디, 푸터 등록록

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer>
+        
+                <h2>Healing Space Company</h2>
+                <p>경기도 성남시 수정구 성남대로 1342</p>
+                <p>&copy; 2024 by Healing Space Company. All rights reserved.</p>
+        
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "../styles/Header.css"
+const Header = () => {
+    return (
+        <header>
+            <nav>
+
+                <ul>
+                    <li>Our Company</li>
+                    <li>Healing Program </li>
+                    <li>News</li>
+                    <li>Customer Service</li>
+                    <li>My Pages</li>
+                    <button>Login</button>
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
