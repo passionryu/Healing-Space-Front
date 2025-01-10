7ac78fcb4875c317cb2d4ff8843ce9342fcb52d4
passionryu <rsy1225@naver.com>
2025-01-10 14:54:13 +0900

    [feat] 메인 로고 클릭 시 메인 화면 랜더링 기능 구현현

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,43 +6,54 @@ const Header = () => {
     return (
         <header>
 
-            {/* 네비게이션 영역 */}
-            <nav>
-                <ul>
-                    <li>
-                        Our Company
-                        <ul className="submenu">
-                            <li><Link to="/about-us">About Us</Link></li>
-                            <li><Link to="/team">Team</Link></li>
-                            
-                        </ul>
-                    </li>
-                    <li>
-                        Healing Program
-                        <ul className="submenu">
-                            <li>Healing Program</li>
-                            <li>Healing Store</li>
-                            <li>AI Recommendation</li>
-                        </ul>
-                    </li>
-                    <li>
-                        News
-                        <ul className="submenu">
-                            <li>Blog</li>
-                            <li>Events</li>
-                            <li>Press</li>
-                        </ul>
-                    </li>
-                    <li>
-                        Customer Service
-                        <ul className="submenu">
-                            <li>Contact Us</li>
-                        </ul>
-                    </li>
-                    <li>My Pages</li>
-                    <button>Login</button>
-                </ul>
-            </nav>
+            <div className="header-container">
+
+                {/* 왼쪽 이미지 영역 */}
+                    <div className="main-logo">
+                        <Link to="/">
+                            <img src="../src/assets/images/main_logo.png" alt="main-logo" />
+                        </Link>
+                    </div>
+
+                {/* 네비게이션 영역 */}
+                <nav>
+                    <ul>
+                        <li>
+                            Our Company
+                            <ul className="submenu">
+                                <li><Link to="/about-us">About Us</Link></li>
+                                <li><Link to="/team">Team</Link></li>
+                                
+                            </ul>
+                        </li>
+                        <li>
+                            Healing Program
+                            <ul className="submenu">
+                                <li>Healing Program</li>
+                                <li>Healing Store</li>
+                                <li>AI Recommendation</li>
+                            </ul>
+                        </li>
+                        <li>
+                            News
+                            <ul className="submenu">
+                                <li>Blog</li>
+                                <li>Events</li>
+                                <li>Press</li>
+                            </ul>
+                        </li>
+                        <li>
+                            Customer Service
+                            <ul className="submenu">
+                                <li>Contact Us</li>
+                            </ul>
+                        </li>
+                        <li>My Pages</li>
+                        <button>Login</button>
+                    </ul>
+                </nav>
+
+            </div>
         </header>
     );
 };
