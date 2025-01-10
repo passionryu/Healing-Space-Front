d2669f392d384af3a1782ebca3602f71b31e519f
passionryu <rsy1225@naver.com>
2025-01-10 16:13:50 +0900

    [feat] healingprogram 페이지 구현현

--- src/pages/HealingProgram.jsx
+++ src/pages/HealingProgram.jsx
@@ -7,18 +7,38 @@ const HealingProgram = () => {
     <div className="healing-program-container">
         
       <div className="card">
-        <h3>Program 1</h3>
-        <p>Description of Program 1</p>
+
+        <div>
+          <img src="../src/assets/images/dewcalendar.png" alt ="이미지"/>
+        </div>
+
+        <div className="card-text">
+          <h3>Dew Calendar</h3>
+          <p>Write your diary...I`ll be with you </p>
+        </div>
+
       </div>
 
       <div className="card">
-        <h3>Program 2</h3>
-        <p>Description of Program 2</p>
+        <div>
+            <img src="../src/assets/images/healingmessagesharing.png" alt ="이미지"/>
+          </div>
+
+          <div className="card-text">
+            <h3>Healing Messge Sharing</h3>
+            <p>Share your Messeage to others</p>
+          </div>
       </div>
 
       <div className="card">
-        <h3>Program 3</h3>
-        <p>Description of Program 3</p>
+        <div>
+            <img src="../src/assets/images/healingmusicsharing.png" alt ="이미지"/>
+          </div>
+
+          <div className="card-text">
+            <h3>Healing Music Sharing</h3>
+            <p>Share Your favorite Music with others</p>
+          </div>
       </div>
 
     </div>
