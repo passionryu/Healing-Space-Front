f6be0cf044df0e610e0943bbcaa59318153f7e53
passionryu <rsy1225@naver.com>
2025-01-10 16:28:34 +0900

    [feat] 메인 페이지에 힐링 프로그램 소개 추가

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -7,6 +7,7 @@ function Main() {
 
     <>
         <div>
+
          {/* SLIDER  */}
             <section>
                 <div className="slider">
@@ -31,6 +32,53 @@ function Main() {
                     </div>
                 </div>
             </section>
+        {/* SLIDER  */}
+
+        {/* 힐링 프로그램 로고 */}
+        <div className='healing-service-logo'>
+            <img src='../src/assets/images/healingservice.png' alt='힐링 서비스 로고'/>
+        </div>
+
+        {/* 힐링 프로그램 요소 */}
+        <div className="healing-program-container">
+        
+        <div className="card">
+  
+          <div>
+            <img src="../src/assets/images/dewcalendar.png" alt ="이미지"/>
+          </div>
+  
+          <div className="card-text">
+            <h3>Dew Calendar</h3>
+            <p>Write your diary...I`ll be with you </p>
+          </div>
+  
+        </div>
+  
+        <div className="card">
+          <div>
+              <img src="../src/assets/images/healingmessagesharing.png" alt ="이미지"/>
+            </div>
+  
+            <div className="card-text">
+              <h3>Healing Messge Sharing</h3>
+              <p>Share your Messeage to others</p>
+            </div>
+        </div>
+  
+        <div className="card">
+          <div>
+              <img src="../src/assets/images/healingmusicsharing.png" alt ="이미지"/>
+            </div>
+  
+            <div className="card-text">
+              <h3>Healing Music Sharing</h3>
+              <p>Share Your favorite Music with others</p>
+            </div>
+        </div>
+  
+      </div>
+            
         </div>
     </>
   );
