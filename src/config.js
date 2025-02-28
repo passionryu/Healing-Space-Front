a06e799dc2ebda2e4466e17f7f9e49ab97e5a1f0
passionryu <rsy1225@naver.com>
2025-03-01 03:09:28 +0900

    [feat] 실제 url OR 로컬 주소로 연결하는 config 파일일

---
+++ src/config.js
@@ -0,0 +1,18 @@
+// const config = {
+//     apiBaseUrl: process.env.REACT_APP_API_BASE_URL || "http://localhost:8080"
+// };
+
+// export default config;
+
+
+/////
+// 아래 코드를 각 페이지 별로 추가하면 됨 
+/////
+
+// import config from "../config";
+
+// const response = await axios.get(`${config.apiBaseUrl}/mypage/myinfo`, {
+//     headers: {
+//         "Authorization": `Bearer ${token}`
+//     }
+// });
