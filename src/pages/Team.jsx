e3fe246202c03f360ed609c5f51d51fe31c9e2fa
passionryu <rsy1225@naver.com>
2025-03-15 23:54:34 +0900

    [feat] team 페이지 모바일 대응

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import "../styles/Team.css"
 
-const Team = () =>{
+const Team = () => {
     return (
         <>
             {/* 이미지 영역 */}
@@ -12,8 +12,12 @@ const Team = () =>{
             <div className='team-recruit'>
                 <h2>Team Recruitment</h2>
                 <p style={{ textAlign: 'left' }}>We are looking for talented team members to help develop a better Healing Space service.</p>
-                <p style={{ textAlign: 'left' }}>If you're interested in working with us, please feel free to contact us.</p>
-                <p style={{ textAlign: 'left' }}>We would appreciate it.</p>
+                <p style={{ textAlign: 'left' }}>If you're interested in working with us, <br />please feel free to contact us.<br />We would appreciate it.</p>
+
+                <p style={{ textAlign: 'left', color: 'gray' }}>
+                    Contact address : rsy1225@naver.com
+                </p>
+
             </div>
 
         </>
